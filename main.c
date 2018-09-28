c1089aa33664db8bce0e30517d3ed40515fb1aa5
Patxii <edgarpatsil@hotmail.com>
2018-09-28 13:46:22 +0200

    fix: Resolved problem in factorial
    Iteration and result both equaled 0 bla bla bla...

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 int factorial (int n) { 
 	//this is fake, dont continue writting
 	int i;
-	int res = 0;
+	int res = 1;
 
 	//No, stop, dont continue with this
-	for (i = 0; i < n; ++i){
+	for (i = 1; i <= n; ++i){
 		res = res + i;
 	}
 
 	//maaaaaan! NOOOO!
-	return 0;
+	return res;
+	//i said, it is not working....
 }
 
 int main(int argc, char const *argv[])
